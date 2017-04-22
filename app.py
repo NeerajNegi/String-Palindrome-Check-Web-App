a7696d4a3f4f70b9dd96673adb53e807848adcf0
NeerajNegi <neeraj.negi174@gmail.com>
2017-04-22 11:33:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
-	return render_template('cloud.html')
+	return 'DONE'
 
 if __name__ == "__main__":
 	app.run()
